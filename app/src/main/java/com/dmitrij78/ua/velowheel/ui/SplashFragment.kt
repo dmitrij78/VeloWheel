e5f373ae18159fc889692997e37ac1699fe78760
Dmitrij Sorokopud <dmitrij.sorokopud@gmail.com>
2020-10-29 23:47:16 +0200

    [Update] Added viewModel to SplashFragment

--- app/src/main/java/com/dmitrij78/ua/velowheel/ui/SplashFragment.kt
+++ app/src/main/java/com/dmitrij78/ua/velowheel/ui/SplashFragment.kt
@@ -1,12 +1,31 @@
 package com.dmitrij78.ua.velowheel.ui
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
 import com.dmitrij78.ua.velowheel.R
 import com.dmitrij78.ua.velowheel.base.BaseFragment
 
 class SplashFragment : BaseFragment() {
 
+    private val viewModel: SplashViewModel by activityViewModels()
+
     companion object {
         fun newInstance() = SplashFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val splashText = view.findViewById<TextView>(R.id.splashText)
+
+        val splashViewModel = ViewModelProvider(this)[SplashViewModel::class.java]
+
+        splashViewModel.splashText.observe(viewLifecycleOwner, { text ->
+            splashText.text = text
+        })
     }
 
     override fun getLayout(): Int = R.layout.fragment_splash
---
+++ app/src/main/java/com/dmitrij78/ua/velowheel/ui/SplashViewModel.kt
@@ -0,0 +1,9 @@
+package com.dmitrij78.ua.velowheel.ui
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel : ViewModel() {
+
+    val splashText = MutableLiveData("SplashText live")
+}
